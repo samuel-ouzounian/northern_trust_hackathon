50551342a4754febbbe62a94a4a7a9b179e34bad
hauska-sam <157626443+hauska-sam@users.noreply.github.com>
2024-10-17 14:18:57 -0500

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
+import StoreProvider from "@/lib/store/storeProvider";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <StoreProvider>
+      <Html lang="en">
+        <Head />
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </StoreProvider>
   );
 }
